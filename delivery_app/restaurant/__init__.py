d7bdd1fef727769b331776af0abddb04609a9ce0
Krzysztof Bogaczyk <bogaczyk.krzysztof00@gmail.com>
2024-05-06 11:31:54 +0200

    initial orders service

--- delivery_app/restaurant/__init__.py
+++ delivery_app/restaurant/__init__.py
@@ -5,8 +5,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 
 restaurant_app = Flask(__name__)
-restaurant_app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
-restaurant_app.config['JWT_SECRET_KEY'] = 'super-secret'
+restaurant_app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+restaurant_app.config["JWT_SECRET_KEY"] = "super-secret"
 
 db = SQLAlchemy(restaurant_app)
 jwt = JWTManager(restaurant_app)
@@ -17,4 +17,4 @@ from .routes import restaurant_bp
 with restaurant_app.app_context():
     db.create_all()
 
-restaurant_app.register_blueprint(restaurant_bp, url_prefix='/restaurant_api')
+restaurant_app.register_blueprint(restaurant_bp, url_prefix="/restaurant_api")
--- run.py
+++ run.py
@@ -1,19 +1,22 @@
 from multiprocessing import Process
 from delivery_app.restaurant import create_restaurant_app
+from delivery_app.order import create_order_app
+
+CREATE_APP_LIST = [create_restaurant_app, create_order_app]
 
 
-def run_app(port):
-    app = create_restaurant_app()
+def run_app(port, service):
+    app = CREATE_APP_LIST[service]()
     app.run(port=port)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     processes = []
 
-    ports = [5001]
+    ports = [[5001, 0], [5002, 1]]
 
     for port in ports:
-        proc = Process(target=run_app, args=(port,))
+        proc = Process(target=run_app, args=(port[0], port[1]))
         proc.start()
         processes.append(proc)
 
