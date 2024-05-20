9522558705e8bdabb7622a01a0ee791d85385126
KrzysztofBialik <105109608+KrzysztofBialik@users.noreply.github.com>
2024-05-20 21:47:36 +0200

    rs256_fix

--- delivery_app/restaurant/__init__.py
+++ delivery_app/restaurant/__init__.py
@@ -6,7 +6,18 @@ from flask_jwt_extended import JWTManager
 
 restaurant_app = Flask(__name__)
 restaurant_app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
-restaurant_app.config['JWT_SECRET_KEY'] = 'super-secret'
+
+private_key_path = os.getenv('JWT_PRIVATE_KEY_PATH')
+public_key_path = os.getenv('JWT_PUBLIC_KEY_PATH')
+
+with open(private_key_path, 'r') as private_key_file:
+    restaurant_app.config['JWT_PRIVATE_KEY'] = private_key_file.read()
+
+with open(public_key_path, 'r') as public_key_file:
+    restaurant_app.config['JWT_PUBLIC_KEY'] = public_key_file.read()
+
+restaurant_app.config['JWT_ALGORITHM'] = 'RS256'
+# restaurant_app.config['JWT_SECRET_KEY'] = 'super-secret'
 
 db = SQLAlchemy(restaurant_app)
 jwt = JWTManager(restaurant_app)
